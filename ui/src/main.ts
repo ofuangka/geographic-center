d11d24ffe6e1ca710c20542d26905377622cf126
Osha Fuangkasae <ofuangka@gmail.com>
2016-09-25 15:25:57 -0500

    fix environment setup

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,7 +2,7 @@ import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { GeographicCenterModule } from './app/geographic-center.module';
-import { environment } from './app';
+import { environment } from './environments/environment';
 
 if (environment.production) {
     enableProdMode();
